8cde3cf7a9803a92ab5ef7ad84e533b375b0f355
puspasari14 <puspasaridiananggraeni@gamil.com>
2020-03-30 06:36:57 +0700

    Initial commit

---
+++ app/src/main/java/com/puspaanggraeni/portofolio/InputFragment.kt
@@ -0,0 +1,40 @@
+package com.puspaanggraeni.portofolio
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_input.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class InputFragment : Fragment() {
+    lateinit var nav :NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_input, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nav = Navigation.findNavController(view)
+
+        buttonsubmit.setOnClickListener {
+            nav.navigate(R.id.action_inputFragment_to_hasilFragment)
+
+        }
+
+    }
+    }
+
+
---
+++ app/src/main/java/com/puspaanggraeni/portofolio/main.kt
@@ -0,0 +1,43 @@
+package com.puspaanggraeni.portofolio
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class main : Fragment() {
+
+    lateinit var nav: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nav = Navigation.findNavController(view)
+
+        buttonutama.setOnClickListener{
+            nav.navigate(R.id.action_main_to_inputFragment)
+
+    }
+
+
+
+    }
+
+
+}
